531b504c32014d673c68cd92634017eca9461cd9
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2021-03-25 15:42:36 -0600

    Dynamic data with network requests

--- app/router.js
+++ app/router.js
@@ -7,7 +7,13 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('teams');
+  this.route('teams', function() {
+    // teams/(lindedIn)*
+    this.route('team', { path: ':teamId'}, function() {
+      // teams/(linkedIn)/(channel)*
+      this.route('channel', { path: ':channelId' });
+    });
+  });
   this.route('login');
 });
 
