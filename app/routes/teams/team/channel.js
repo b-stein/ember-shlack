638ec8bcc221d3c8adbff57d181e0729e4deea99
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2021-03-26 11:31:47 -0600

    Container components implemented

--- app/routes/teams/team/channel.js
+++ app/routes/teams/team/channel.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 
 export default class TeamsTeamChannelRoute extends Route {
+  //TODO: Where does { channelId } come from ??
   async model({ channelId }) {
     const { teamId } = this.paramsFor('teams.team');
     const response = await fetch(`/api/teams/${teamId}/channels/${channelId}`);
